fdbbbcfa68cb9a4d9f57495710c8d6cfc950e918
Oleg Kosheliev <oleg.kosheliev@globallogic.com>
2016-10-12 12:55:57 +0300

    drm: rcar-du: Set defalut VGA resolution to 1920x1080
    Change-Id: I0983ff2c6ed065eefeaca4ffd4ea09435f62ad10
    Signed-off-by: Oleg Kosheliev <oleg.kosheliev@globallogic.com>

--- drivers/gpu/drm/rcar-du/rcar_du_vgacon.c
+++ drivers/gpu/drm/rcar-du/rcar_du_vgacon.c
@@ -23,7 +23,7 @@
 
 static int rcar_du_vga_connector_get_modes(struct drm_connector *connector)
 {
-	return drm_add_modes_noedid(connector, 1024, 768);
+	return drm_add_modes_noedid(connector, 1920, 1080);
 }
 
 static const struct drm_connector_helper_funcs connector_helper_funcs = {

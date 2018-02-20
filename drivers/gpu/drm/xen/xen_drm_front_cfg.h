73acbf3fe4c2ae2b5903b7157b495cf08747870a
Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
2018-02-20 11:05:25 +0200

    drm/xen-front: Read driver configuration from Xen store
    Read configuration values from Xen store according
    to xen/interface/io/displif.h protocol:
      - read connector(s) configuration
      - read buffer allocation mode (backend/frontend)
    
    Signed-off-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>

--- drivers/gpu/drm/xen/xen_drm_front_cfg.h
+++ drivers/gpu/drm/xen/xen_drm_front_cfg.h
@@ -16,14 +16,30 @@
  * Author: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
  */
 
-#ifndef __XEN_DRM_FRONT_H_
-#define __XEN_DRM_FRONT_H_
+#ifndef __XEN_DRM_FRONT_CFG_H_
+#define __XEN_DRM_FRONT_CFG_H_
 
-#include "xen_drm_front_cfg.h"
+#include <linux/types.h>
 
-struct xen_drm_front_info {
-	struct xenbus_device *xb_dev;
-	struct xen_drm_front_cfg cfg;
+#define XEN_DRM_FRONT_MAX_CRTCS	4
+
+struct xen_drm_front_cfg_connector {
+	int width;
+	int height;
+	char *xenstore_path;
 };
 
-#endif /* __XEN_DRM_FRONT_H_ */
+struct xen_drm_front_cfg {
+	struct xen_drm_front_info *front_info;
+	/* number of connectors in this configuration */
+	int num_connectors;
+	/* connector configurations */
+	struct xen_drm_front_cfg_connector connectors[XEN_DRM_FRONT_MAX_CRTCS];
+	/* set if dumb buffers are allocated externally on backend side */
+	bool be_alloc;
+};
+
+int xen_drm_front_cfg_card(struct xen_drm_front_info *front_info,
+		struct xen_drm_front_cfg *cfg);
+
+#endif /* __XEN_DRM_FRONT_CFG_H_ */

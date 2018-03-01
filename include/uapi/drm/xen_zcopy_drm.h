6fe13f7d33cb0938c4fc5b8e602001d5b5e112ac
Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
2018-03-01 12:31:44 +0200

    drm/xen-zcopy: Introduce Xen zero-copy helper DRM driver
    Introduce skeleton of the Xen zero-copy helper DRM driver,
    add user-space API of the driver:
    
    1. DRM_IOCTL_XEN_ZCOPY_DUMB_FROM_REFS
    This will create a DRM dumb buffer from grant references provided
    by the frontend. The intended usage is:
      - Frontend
        - creates a dumb/display buffer and allocates memory
        - grants foreign access to the buffer pages
        - passes granted references to the backend
      - Backend
        - issues DRM_XEN_ZCOPY_DUMB_FROM_REFS ioctl to map
          granted references and create a dumb buffer
        - requests handle to fd conversion via DRM_IOCTL_PRIME_HANDLE_TO_FD
        - requests real HW driver/consumer to import the PRIME buffer with
          DRM_IOCTL_PRIME_FD_TO_HANDLE
        - uses handle returned by the real HW driver
      - at the end:
        o closes real HW driver's handle with DRM_IOCTL_GEM_CLOSE
        o closes zero-copy driver's handle with DRM_IOCTL_GEM_CLOSE
        o closes file descriptor of the exported buffer
    
    2. DRM_IOCTL_XEN_ZCOPY_DUMB_TO_REFS
    This will grant references to a dumb/display buffer's memory provided by the
    backend. The intended usage is:
      - Frontend
        - requests backend to allocate dumb/display buffer and grant references
          to its pages
      - Backend
        - requests real HW driver to create a dumb with DRM_IOCTL_MODE_CREATE_DUMB
        - requests handle to fd conversion via DRM_IOCTL_PRIME_HANDLE_TO_FD
        - requests zero-copy driver to import the PRIME buffer with
          DRM_IOCTL_PRIME_FD_TO_HANDLE
        - issues DRM_XEN_ZCOPY_DUMB_TO_REFS ioctl to
          grant references to the buffer's memory.
        - passes grant references to the frontend
     - at the end:
        - closes zero-copy driver's handle with DRM_IOCTL_GEM_CLOSE
        - closes real HW driver's handle with DRM_IOCTL_GEM_CLOSE
        - closes file descriptor of the imported buffer
    
    Signed-off-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>

---
+++ include/uapi/drm/xen_zcopy_drm.h
@@ -0,0 +1,91 @@
+/* SPDX-License-Identifier: GPL-2.0 WITH Linux-syscall-note */
+
+/*
+ *  Xen zero-copy helper DRM device
+ *
+ * Copyright (C) 2016-2018 EPAM Systems Inc.
+ *
+ * Author: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
+ */
+#ifndef __XEN_ZCOPY_DRM_H
+#define __XEN_ZCOPY_DRM_H
+
+#include "drm.h"
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+#define XENDRM_ZCOPY_DRIVER_NAME	"xen_drm_zcopy"
+
+/*
+ * Xen DRM zero copy specific ioctls.
+ *
+ * This will create a DRM dumb buffer from grant references provided
+ * by the frontend:
+ *  o Frontend
+ *    o creates a dumb/display buffer and allocates memory.
+ *    o grants foreign access to the buffer pages
+ *    o passes granted references to the backend
+ *  o Backend
+ *    o issues DRM_XEN_ZCOPY_DUMB_FROM_REFS ioctl to map
+ *      granted references and create a dumb buffer.
+ *    o requests handle to fd conversion via DRM_IOCTL_PRIME_HANDLE_TO_FD
+ *    o requests real HW driver to import the PRIME buffer with
+ *      DRM_IOCTL_PRIME_FD_TO_HANDLE
+ *    o uses handle returned by the real HW driver
+ *    o at the end:
+ *      o closes real HW driver's handle with DRM_IOCTL_GEM_CLOSE
+ *      o closes zero-copy driver's handle with DRM_IOCTL_GEM_CLOSE
+ *      o closes file descriptor of the exported buffer
+ */
+#define DRM_XEN_ZCOPY_DUMB_FROM_REFS	0x00
+
+struct drm_xen_zcopy_dumb_from_refs {
+	uint32_t num_grefs;
+	/* user-space uses uint32_t instead of grant_ref_t for mapping */
+	uint32_t *grefs;
+	uint64_t otherend_id;
+	struct drm_mode_create_dumb dumb;
+};
+
+/*
+ * This will grant references to a dumb/display buffer's memory provided by the
+ * backend:
+ *  o Frontend
+ *    o requests backend to allocate dumb/display buffer and grant references
+ *      to its pages
+ *  o Backend
+ *    o requests real HW driver to create a dumb with DRM_IOCTL_MODE_CREATE_DUMB
+ *    o requests handle to fd conversion via DRM_IOCTL_PRIME_HANDLE_TO_FD
+ *    o requests zero-copy driver to import the PRIME buffer with
+ *      DRM_IOCTL_PRIME_FD_TO_HANDLE
+ *    o issues DRM_XEN_ZCOPY_DUMB_TO_REFS ioctl to
+ *      grant references to the buffer's memory.
+ *   o passes grant references to the frontend
+ *   o at the end:
+ *     o closes zero-copy driver's handle with DRM_IOCTL_GEM_CLOSE
+ *     o closes real HW driver's handle with DRM_IOCTL_GEM_CLOSE
+ *     o closes file descriptor of the imported buffer
+ */
+#define DRM_XEN_ZCOPY_DUMB_TO_REFS	0x01
+
+struct drm_xen_zcopy_dumb_to_refs {
+	uint32_t num_grefs;
+	/* user-space uses uint32_t instead of grant_ref_t for mapping */
+	uint32_t *grefs;
+	uint64_t otherend_id;
+	uint32_t handle;
+};
+
+#define DRM_IOCTL_XEN_ZCOPY_DUMB_FROM_REFS DRM_IOWR(DRM_COMMAND_BASE + \
+	DRM_XEN_ZCOPY_DUMB_FROM_REFS, struct drm_xen_zcopy_dumb_from_refs)
+
+#define DRM_IOCTL_XEN_ZCOPY_DUMB_TO_REFS DRM_IOWR(DRM_COMMAND_BASE + \
+	DRM_XEN_ZCOPY_DUMB_TO_REFS, struct drm_xen_zcopy_dumb_to_refs)
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif /* __XEN_ZCOPY_DRM_H*/

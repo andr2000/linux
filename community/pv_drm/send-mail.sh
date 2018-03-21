2c0280be9cf65eee8e925c02f7babcddc38ada28
Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
2018-03-21 17:03:34 +0200

    pv drm v3
    Signed-off-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>

--- community/pv_drm/send-mail.sh
+++ community/pv_drm/send-mail.sh
@@ -1,2 +1,2 @@
-git send-email --to xen-devel@lists.xenproject.org --to linux-kernel@vger.kernel.org --to dri-devel@lists.freedesktop.org --to airlied@linux.ie --to daniel.vetter@intel.com --to seanpaul@chromium.org --to gustavo@padovan.org --to jgross@suse.com --to boris.ostrovsky@oracle.com --to konrad.wilk@oracle.com --cc=andr2000@gmail.com community/pv_drm/v2_resend
+git send-email --to xen-devel@lists.xenproject.org --to linux-kernel@vger.kernel.org --to dri-devel@lists.freedesktop.org --to airlied@linux.ie --to daniel.vetter@intel.com --to seanpaul@chromium.org --to gustavo@padovan.org --to jgross@suse.com --to boris.ostrovsky@oracle.com --to konrad.wilk@oracle.com --cc=andr2000@gmail.com community/pv_drm/v3
 

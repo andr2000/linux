d953826805ee118c8811c7bc2f7e393120b1181d
Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
2018-05-22 17:14:02 +0300

    drm unplug v1
    Signed-off-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>

---
+++ community/fix_drm_unplug/send-mail.sh
@@ -0,0 +1,2 @@
+git send-email --to linux-kernel@vger.kernel.org --to dri-devel@lists.freedesktop.org --to airlied@linux.ie --to daniel.vetter@intel.com --to seanpaul@chromium.org --to gustavo@padovan.org --cc=andr2000@gmail.com community/fix_drm_unplug/v1
+

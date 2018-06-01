354f04c940ef1757acca16bca9f5947370133b2f
Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
2018-06-01 14:42:16 +0300

    dma-buf v2
    Signed-off-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>

--- community/dma_buf/send-mail.sh
+++ community/dma_buf/send-mail.sh
@@ -1,2 +1,2 @@
-git send-email --to xen-devel@lists.xenproject.org --to linux-kernel@vger.kernel.org --to dri-devel@lists.freedesktop.org --to linux-media@vger.kernel.org --cc daniel.vetter@intel.com --to jgross@suse.com --to boris.ostrovsky@oracle.com --to konrad.wilk@oracle.com --cc=andr2000@gmail.com --cc=dongwon.kim@intel.com --cc=matthew.d.roper@intel.com community/dma_buf/v1
+git send-email --to xen-devel@lists.xenproject.org --to linux-kernel@vger.kernel.org --to dri-devel@lists.freedesktop.org --to linux-media@vger.kernel.org --cc daniel.vetter@intel.com --to jgross@suse.com --to boris.ostrovsky@oracle.com --to konrad.wilk@oracle.com --cc=andr2000@gmail.com --cc=dongwon.kim@intel.com --cc=matthew.d.roper@intel.com community/dma_buf/v2
 

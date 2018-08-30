e9f841a15d01db456f17f4cf469871455f24ee75
Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
2018-08-30 14:16:47 +0300

    Test event channel
    Signed-off-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>

--- drivers/media/xen/xen_camera_front.c
+++ drivers/media/xen/xen_camera_front.c
@@ -20,6 +20,65 @@
 #include "xen_camera_front.h"
 #include "xen_camera_front_evtchnl.h"
 
+static struct xencamera_req *
+be_prepare_req(struct xen_camera_front_evtchnl *evtchnl, u8 operation)
+{
+	struct xencamera_req *req;
+
+	req = RING_GET_REQUEST(&evtchnl->u.req.ring,
+			       evtchnl->u.req.ring.req_prod_pvt);
+	req->operation = operation;
+	req->id = evtchnl->evt_next_id++;
+	evtchnl->evt_id = req->id;
+	return req;
+}
+
+static int be_stream_do_io(struct xen_camera_front_evtchnl *evtchnl,
+			   struct xencamera_req *req)
+{
+	reinit_completion(&evtchnl->u.req.completion);
+	if (unlikely(evtchnl->state != EVTCHNL_STATE_CONNECTED))
+		return -EIO;
+
+	xen_camera_front_evtchnl_flush(evtchnl);
+	return 0;
+}
+
+static int be_stream_wait_io(struct xen_camera_front_evtchnl *evtchnl)
+{
+	if (wait_for_completion_timeout(&evtchnl->u.req.completion,
+			msecs_to_jiffies(XEN_CAMERA_FRONT_WAIT_BACK_MS)) <= 0)
+		return -ETIMEDOUT;
+
+	return evtchnl->u.req.resp_status;
+}
+
+static int xen_camera_test(struct xen_camera_front_info *front_info)
+{
+	struct xen_camera_front_evtchnl *evtchnl;
+	struct xencamera_req *req;
+	unsigned long flags;
+	int ret;
+
+	evtchnl = &front_info->evt_pair.req;
+	if (unlikely(!evtchnl))
+		return -EIO;
+
+	mutex_lock(&evtchnl->u.req.req_io_lock);
+
+	spin_lock_irqsave(&front_info->io_lock, flags);
+	req = be_prepare_req(evtchnl, XENCAMERA_OP_SET_CONFIG);
+
+	ret = be_stream_do_io(evtchnl, req);
+	spin_unlock_irqrestore(&front_info->io_lock, flags);
+
+	if (ret == 0)
+		ret = be_stream_wait_io(evtchnl);
+
+	mutex_unlock(&evtchnl->u.req.req_io_lock);
+	return ret;
+}
+
 static void xen_camera_drv_fini(struct xen_camera_front_info *front_info)
 {
 	xen_camera_front_evtchnl_free_all(front_info);
@@ -81,6 +140,7 @@ static void cameraback_changed(struct xenbus_device *xb_dev,
 			xenbus_dev_fatal(xb_dev, ret, "initializing frontend");
 		else
 			xenbus_switch_state(xb_dev, XenbusStateInitialised);
+
 		break;
 
 	case XenbusStateConnected:
@@ -92,6 +152,9 @@ static void cameraback_changed(struct xenbus_device *xb_dev,
 			xenbus_dev_fatal(xb_dev, ret, "initializing frontend");
 		else
 			xenbus_switch_state(xb_dev, XenbusStateConnected);
+
+		xen_camera_front_evtchnl_pair_set_connected(&front_info->evt_pair, true);
+		xen_camera_test(front_info);
 		break;
 
 	case XenbusStateClosing:
@@ -124,6 +187,7 @@ static int xen_drv_probe(struct xenbus_device *xb_dev,
 		return -ENOMEM;
 
 	front_info->xb_dev = xb_dev;
+	spin_lock_init(&front_info->io_lock);
 	dev_set_drvdata(&xb_dev->dev, front_info);
 
 	return xenbus_switch_state(xb_dev, XenbusStateInitialising);
--- drivers/media/xen/xen_camera_front.h
+++ drivers/media/xen/xen_camera_front.h
@@ -17,6 +17,9 @@ struct xen_camera_front_info {
 	struct xenbus_device *xb_dev;
 
 	struct xen_camera_front_evtchnl_pair evt_pair;
+
+	/* to protect data between backend IO code and interrupt handler */
+	spinlock_t io_lock;
 };
 
 #endif /* __XEN_CAMERA_FRONT_H */

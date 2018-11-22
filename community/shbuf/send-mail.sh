1bdb6959e6beb46e055439ab727219daa36a268d
Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
2018-11-22 12:05:06 +0200

    shbuf v1
    Signed-off-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>

---
+++ community/shbuf/send-mail.sh
@@ -0,0 +1,2 @@
+git send-email --to xen-devel@lists.xenproject.org --to linux-kernel@vger.kernel.org --to dri-devel@lists.freedesktop.org --to alsa-devel@alsa-project.org --to jgross@suse.com --to boris.ostrovsky@oracle.com --cc=andr2000@gmail.com community/shbuf/v1
+

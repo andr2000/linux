bf8d73ae88d93547566e74fa0ed83b99c1e1e199
Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
2019-01-15 15:47:06 +0200

    drm fix shmem v2 (flickering)

--- community/drm_fix_shmem/send-mail.sh
+++ community/drm_fix_shmem/send-mail.sh
@@ -1,2 +1,2 @@
-git send-email --to xen-devel@lists.xenproject.org --to linux-kernel@vger.kernel.org --to dri-devel@lists.freedesktop.org --to daniel.vetter@intel.com --to jgross@suse.com --to boris.ostrovsky@oracle.com --cc=andr2000@gmail.com community/drm_fix_shmem/v1
+git send-email --to xen-devel@lists.xenproject.org --to linux-kernel@vger.kernel.org --to dri-devel@lists.freedesktop.org --to daniel.vetter@intel.com --to jgross@suse.com --to boris.ostrovsky@oracle.com --to kraxel@redhat.com --to noralf@tronnes.org --to hch@infradead.org --cc=andr2000@gmail.com community/drm_fix_shmem/v2
 

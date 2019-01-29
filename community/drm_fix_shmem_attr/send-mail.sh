c157507128cd5930ee8cd2a2f74129f4ec95ec63
Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
2019-01-29 17:04:52 +0200

    drm fix shmem attr v1 (flickering)
    Signed-off-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>

---
+++ community/drm_fix_shmem_attr/send-mail.sh
@@ -0,0 +1,2 @@
+git send-email --to xen-devel@lists.xenproject.org --to linux-kernel@vger.kernel.org --to dri-devel@lists.freedesktop.org --to daniel.vetter@intel.com --to jgross@suse.com --to boris.ostrovsky@oracle.com --to kraxel@redhat.com --to noralf@tronnes.org --to hch@infradead.org --to julien.grall@arm.com --cc=andr2000@gmail.com community/drm_fix_shmem_attr/v1
+

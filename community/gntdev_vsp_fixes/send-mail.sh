c7f657fc5c5bf3cae1b578eb334fc31f1fce86fa
Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
2019-03-14 15:18:56 +0200

    vsp fixes

---
+++ community/gntdev_vsp_fixes/send-mail.sh
@@ -0,0 +1,2 @@
+git send-email --to xen-devel@lists.xenproject.org --to linux-kernel@vger.kernel.org --to jgross@suse.com --to boris.ostrovsky@oracle.com --cc=andr2000@gmail.com community/gntdev_vsp_fixes/v1
+

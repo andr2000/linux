5fe7c42c1261d937319588949de299fde9a483b3
Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
2019-04-04 15:42:04 +0300

    fix vsnd buffer size v1

---
+++ community/snd_fix_bufsz/send-mail.sh
@@ -0,0 +1,2 @@
+git send-email --to xen-devel@lists.xenproject.org --to linux-kernel@vger.kernel.org --to alsa-devel@alsa-project.org --to jgross@suse.com --to boris.ostrovsky@oracle.com --to konrad.wilk@oracle.com --to perex@perex.cz --to tiwai@suse.com --cc=andr2000@gmail.com community/snd_fix_bufsz/v1/
+

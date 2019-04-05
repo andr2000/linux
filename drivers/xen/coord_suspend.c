276ee878c138c6ee46119f4f075f5a31bfa27889
Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
2019-04-05 10:55:19 +0300

    zynqmp: Fix coord driver compilation error
    Fix coord driver compilation on newer kernels:
    
    drivers/xen/coord_suspend.c:97:34: error: array type has incomplete element type ‘struct of_device_id’
     static const struct of_device_id coord_susp_of_match[] = {
    
    This is due to:
    ac3167257b9f ("headers: separate linux/mod_devicetable.h from linux/platform_device.h")
    
    Signed-off-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>

--- drivers/xen/coord_suspend.c
+++ drivers/xen/coord_suspend.c
@@ -9,6 +9,7 @@
 #include <linux/init.h>
 #include <linux/interrupt.h>
 #include <linux/module.h>
+#include <linux/mod_devicetable.h>
 #include <linux/platform_device.h>
 #include <linux/suspend.h>
 

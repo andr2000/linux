f54f599c1b9335eb2c0c9abae27b419aa5ba91af
Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
2020-07-31 15:35:07 +0300

    edid v1

---
+++ community/edid/edid-send.sh
@@ -0,0 +1,7 @@
+git send-email \
+	--to xen-devel@lists.xenproject.org --to dri-devel@lists.freedesktop.org --to linux-kernel@vger.kernel.org \
+	--to boris.ostrovsky@oracle.com --to jgross@suse.com --cc=sstabellini@kernel.org \
+	--to airlied@linux.ie --to daniel@ffwll.ch \
+	--cc=dan.carpenter@oracle.com \
+	--cc=intel-gfx@lists.freedesktop.org \
+	community/edid/v1

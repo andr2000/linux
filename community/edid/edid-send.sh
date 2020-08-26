5dc8ae28772a83cef753556fede6cbc0adf0ccd2
Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
2020-08-26 17:01:34 +0300

    edid as single patch v1

--- community/edid/edid-send.sh
+++ community/edid/edid-send.sh
@@ -1,7 +1,4 @@
 git send-email \
-	--to xen-devel@lists.xenproject.org --to dri-devel@lists.freedesktop.org --to linux-kernel@vger.kernel.org \
-	--to boris.ostrovsky@oracle.com --to jgross@suse.com --cc=sstabellini@kernel.org \
-	--to airlied@linux.ie --to daniel@ffwll.ch \
-	--cc=dan.carpenter@oracle.com \
+	--to dri-devel@lists.freedesktop.org --to linux-kernel@vger.kernel.org \
 	--cc=intel-gfx@lists.freedesktop.org \
-	community/edid/v2
+	community/edid/single_patch_v1/

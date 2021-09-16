d91c631cf34e884cf2222aa019db66f3e863aa6e
Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
2021-09-16 12:33:50 +0300

    xen-pciback: allow compiling on other archs than x86
    Xen-pciback driver was designed to be built for x86 only. But it
    can also be used by other architectures, e.g. Arm.
    Re-structure the driver in a way that it can be built for other
    platforms as well.
    
    Add new configuration option CONFIG_XEN_PCIDEV_STUB, so the driver
    can be limited in its functionality, e.g. no support for
    para-virtualised scenario when it acts as PCI backend.
    x86 platform will continue using CONFIG_XEN_PCIDEV_BACKEND
    for the fully featured backend driver.
    
    Signed-off-by: Oleksandr Andrushchenko <oleksandr_andrushchenko@epam.com>
    Signed-off-by: Anastasiia Lukianenko <anastasiia_lukianenko@epam.com>
    
    ---
    Since v3:
     - Fix >>32 shift for 32-bit architectures
     - Move CONFIG_XEN_PCIDEV_STUB into this patch
    Since v2:
     - swap the patch order
    Since v1:
     - Do not move pci_xen_initial_domain as it is x86 specific

--- drivers/xen/xen-pciback/xenbus.c
+++ drivers/xen/xen-pciback/xenbus.c
@@ -14,7 +14,7 @@
 #include <linux/workqueue.h>
 #include <xen/xenbus.h>
 #include <xen/events.h>
-#include <asm/xen/pci.h>
+#include <xen/pci.h>
 #include "pciback.h"
 
 #define INVALID_EVTCHN_IRQ  (-1)
